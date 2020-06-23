4b9a86e9fc06df23da032dfa8cc0c9af7d35d840
Root <root@kakaopage.com>
2020-06-23 23:16:51 +0900

    Updates

---
+++ precache-manifest.00db3f2c8a3286bf86db3cdb565861bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3db825d305852b5aa51ad7b9d3456b3",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "cca64b2cb4e3eec507b3",
+    "url": "/deploy/static/css/main.3b78be1e.chunk.css"
+  },
+  {
+    "revision": "902b586e068fc3609209",
+    "url": "/deploy/static/js/2.9ecd359c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deploy/static/js/2.9ecd359c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca64b2cb4e3eec507b3",
+    "url": "/deploy/static/js/main.c8a27317.chunk.js"
+  },
+  {
+    "revision": "2877f3a407a81b5bcf24",
+    "url": "/deploy/static/js/runtime-main.b97e59d6.js"
+  }
+]);
